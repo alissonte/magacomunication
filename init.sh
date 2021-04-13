4904426ffefccd5d0af7804e560772949ede7c0a
Alisson Rodrigues <alissonte@gmail.com>
2021-04-13 15:39:25 -0300

    Script de espera do DB

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -eu
+echo "Checking DB connection ..."
+ 
+i=0
+until [ $i -ge 20 ]
+do
+  nc -z 172.21.0.2 3306 && break
+ 
+  i=$(( i + 1 ))
+ 
+  echo "$i: Waiting for DB 1 second ..."
+  sleep 1
+done
+ 
+if [ $i -eq 20 ]
+then
+  echo "DB connection refused, terminating ..."
+  exit 1
+fi
+ 
+echo "DB is up ..."
+ 
+java-jar build/libs/maga.jar
